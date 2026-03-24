62a405e88e94cbce987d513b899b3a44aed6df9b
Popsiclette <niasimagalhaes@gmail.com>
2026-03-24 18:20:38 -0300

    Fix indentation to comply with detekt rules

--- src/main/kotlin/com/github/getomni/jetbrains/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/getomni/jetbrains/services/MyApplicationService.kt
@@ -3,7 +3,7 @@ package com.github.getomni.jetbrains.services
 import com.github.getomni.jetbrains.MyBundle
 
 class MyApplicationService {
-  init {
-    println(MyBundle.message("applicationService"))
-  }
+    init {
+        println(MyBundle.message("applicationService"))
+    }
 }
--- src/main/kotlin/com/github/getomni/jetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/getomni/jetbrains/services/MyProjectService.kt
@@ -4,7 +4,7 @@ import com.github.getomni.jetbrains.MyBundle
 import com.intellij.openapi.project.Project
 
 class MyProjectService(project: Project) {
-  init {
-    println(MyBundle.message("projectService", project.name))
-  }
+    init {
+        println(MyBundle.message("projectService", project.name))
+    }
 }
